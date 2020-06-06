f77bc1cdc5784fcd8bb8b1418fa2d15e0848cb8d
Diogo Naka <diogoshindhinaka@gmail.com>
2020-06-06 19:56:26 -0300

    Project NLW (Web, Mobile & Server)

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import path from 'path';
+import { errors } from 'celebrate';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors());
+
+app.listen(3333, () => {
+  console.log('Server started 🚀')
+})
